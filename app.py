ed8811ac28d56fb18f58e84400d2759f30895ea6
Marc Gravel <gravel.m@outlook.com>
2021-12-20 22:07:47 -0800

    created player and ship placement function

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from gameboard import GameBoard
+from player import Player
 
-def PlayGame():
-    pOneBoard = GameBoard().gameboard
-    pTwoBoard = GameBoard().gameboard
+def play_game():
+    player_one = Player()
+    player_two = Player()
     
-    print(pOneBoard)
+    print("-------------------")
+    print("Welcome to BattleShip, 2-player mode.")
+    print("Ships have a length of 3")
+    print("-------------------")
+    
+    print("Player One, Please choose the center coordinate for your ship")
+    print(" ")
+    player_one.board = Player.set_ship_coordinates()
+    print(player_one.board)
+    
+    
 if __name__ == '__main__':
-    PlayGame()
+    play_game()
---
+++ player.py
@@ -0,0 +1,73 @@
+from gameboard import GameBoard
+
+class Player():
+    def __init__(self):
+        self.board = GameBoard().gameboard
+    
+    def set_ship_coordinates():
+        letterArray = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        board = GameBoard().gameboard
+        
+        while True:
+        
+            while True:
+                direction_input = input("Which direction? Horizontal 'H' or Vertical 'V': ")
+                upper_direction = direction_input.upper() #uppercase the input for the function check
+                
+                if upper_direction == "H" or upper_direction == "V":
+                    break
+                else:
+                    print("That is not a valid input, please enter H or V")
+                    print(" ")
+            
+            while True: 
+                row_input = input("Which row? (Between 1 and 8): ")
+                try:
+                    int_row = int(row_input)
+                    if int_row >= 1 and int_row <=8:
+                        break
+                    else:
+                        print("That is not a valid input, please enter between 1 and 8")
+                        print(" ")
+                except ValueError:
+                    print("This is not a valid response")
+                    
+            while True: 
+                column_input = input("Which Column? (Between A and H): ")
+                upper_column = column_input.upper() #uppercase the input for loop check
+                
+                if upper_column in letterArray:
+                    column_choice = letterArray.index(upper_column)
+                    break
+                else:
+                    print("That is not a valid input, please enter letter between A and H")
+                    print(" ")
+            
+            dir = upper_direction
+            row = int_row - 1 #remove one here to account for 0 in list 
+            column = column_choice #this is already an index value and does not need to -1
+            
+            if dir == "H":
+                #if horizontal, column cannot be 0 or 7 at center or ship is out of bounds
+                if column != 0 and column != 7:
+                    #center of ship
+                    board[row][column] = "O"
+                    #front and back of ship
+                    board[row][column - 1] = "O"
+                    board[row][column + 1] = "O"
+                    return board
+                else: 
+                    print("Can't place ship here, try again")
+            elif dir == "V":
+                #if vertical, row cannot be 0 or 7 at center or ship is out of bounds
+                if row != 0 and row != 7:
+                    #center of ship
+                    board[row][column] = "O"
+                    #front and back of ship
+                    board[row - 1][column] = "O"
+                    board[row + 1][column] = "O"
+                    return board
+                else: 
+                    print("Can't place ship here, try again")
+            else:
+                print("Something went wrong")
